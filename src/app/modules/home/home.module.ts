b1703d77ee41fcbd44c1c48edc9be693e9291b77
Manuchar Chachanidze <manucharch@gmail.com>
2024-03-12 15:25:28 +0400

    added footer

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,18 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { HomeComponent } from 'src/app/modules/home/components/home/home.component';
 
-// const routes: Routes = [
-//   { path: '', redirectTo: 'home', pathMatch: 'full' },
-//   {
-//     path: 'home',
-//     component: HomeComponent,
-//     children: [
-//       { path: 'abought', component: AboughtComponent },
-//       { path: 'projects', component: ProjectsComponent },
-//     ],
-//   },
-// ];
-
 @NgModule({
   declarations: [HomeComponent],
   imports: [CommonModule],
