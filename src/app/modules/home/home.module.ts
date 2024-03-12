067aba4273ecafbcc55d29335850d31b58891a3f
Manuchar Chachanidze <manucharch@gmail.com>
2024-03-12 10:54:02 +0400

    added navbar

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from './components/home/home.component';
 
+import { HomeComponent } from 'src/app/modules/home/components/home/home.component';
+import { NavbarComponent } from 'src/app/components/navbar/navbar.component';
+import { FooterComponent } from 'src/app/components/footer/footer.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+];
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HomeComponent, NavbarComponent, FooterComponent],
+  imports: [CommonModule, RouterModule.forChild(routes)],
 })
-export class HomeModule { }
+export class HomeModule {}
