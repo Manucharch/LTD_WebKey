f6b6d50c358946b662fe2c948f893e785c99dd84
Manuchar Chachanidze <manucharch@gmail.com>
2024-03-14 11:47:48 +0400

    Home component - checkpoint 1

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,11 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { HomeComponent } from 'src/app/modules/home/components/home/home.component';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpTranslateLoader,
+        deps: [HttpClient],
+      },
+    }),
+  ],
 })
 export class HomeModule {}
+
+export function httpTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
