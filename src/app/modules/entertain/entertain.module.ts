52fbab26c1fb0ebfc5966c3aadfedc737d519a07
Manuchar Chachanidze <manucharch@gmail.com>
2024-04-19 16:01:55 +0400

    unit table done

--- src/app/modules/entertain/entertain.module.ts
+++ src/app/modules/entertain/entertain.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { EntertainComponent } from 'src/app/modules/entertain/components/entertain/entertain.component';
-import { UnittableComponent } from 'src/app/modules/entertain/components/unittable/unittable.component';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { HttpClient } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+import { EntertainComponent } from 'src/app/modules/entertain/components/entertain/entertain.component';
+
+import { UnittableComponent } from 'src/app/modules/entertain/components/unittable/unittable.component';
 
 @NgModule({
   declarations: [EntertainComponent, UnittableComponent],
