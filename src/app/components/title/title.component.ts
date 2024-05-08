85cca12c4d491fccb41c28bbbfae74002fdf79ac
Manuchar Chachanidze <manucharch@gmail.com>
2024-05-08 17:15:18 +0400

    added some fonts

--- src/app/components/title/title.component.ts
+++ src/app/components/title/title.component.ts
@@ -1,9 +1,18 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.scss'],
+  animations: [
+    trigger('scaleOnInit', [
+      transition(':enter', [
+        style({ transform: 'scale(0, 1)' }),
+        animate('500ms ease-out', style({ transform: 'scale(1, 1)' }))
+      ])
+    ])
+  ]
 })
 export class TitleComponent implements AfterViewInit {
   backgroundImageUrl: string = '../../../assets/img/flags/ge.svg';
--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -4,24 +4,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { HttpClient } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { MainComponent } from 'src/app/modules/main/components/main/main.component';
 import { NavbarComponent } from 'src/app/components/navbar/navbar.component';
 import { FooterComponent } from 'src/app/components/footer/footer.component';
 import { HomeComponent } from 'src/app/modules/home/components/home/home.component';
-import { AboughtComponent } from 'src/app/modules/abought/components/abought/abought.component';
-import { ProjectsComponent } from 'src/app/modules/projects/components/projects/projects.component';
-import { ContactComponent } from 'src/app/modules/contact/components/contact/contact.component';
 import { HomeModule } from 'src/app/modules/home/home.module';
 import { TitleComponent } from 'src/app/components/title/title.component';
-import { AboughtModule } from 'src/app/modules/abought/abought.module';
-import { ContactModule } from 'src/app/modules/contact/contact.module';
-import { ReactiveFormsModule } from '@angular/forms';
-import { ProjectsModule } from 'src/app/modules/projects/projects.module';
-import { BioComponent } from 'src/app/modules/bio/components/bio/bio.component';
-import { BioModule } from 'src/app/modules/bio/bio.module';
-import { EntertainComponent } from 'src/app/modules/entertain/components/entertain/entertain.component';
-import { EntertainModule } from 'src/app/modules/entertain/entertain.module';
 
 const routes: Routes = [
   {
@@ -55,7 +47,8 @@ const routes: Routes = [
     }),
     RouterModule.forChild(routes),
     ReactiveFormsModule,
-    HomeModule
+    HomeModule,
+    BrowserAnimationsModule
   ],
 })
 export class MainModule {}
